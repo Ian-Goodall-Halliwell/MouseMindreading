9030fa9c6f79d9848c967af63325a10c8c84d932
phno <philipp.norton@hu-berlin.de>
2023-07-21 17:39:50 +0200

    moved plotting to new file plots.py

--- rebin.py
+++ rebin.py
@@ -1,21 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import plots
 
 # load spike data (run download_lfp.py first)
-dat_st = np.load("dat_st.npy", allow_pickle=True)
+#dat_st = np.load("dat_st.npy", allow_pickle=True)
+# todo: just loading first session to speed up testing
+dat_st = np.load("dat_test_s0.npy", allow_pickle=True)
+i_session = 0
+spiketimes = dat_st[i_session]['ss']
 
-# test: plot spikes from first session, first trial for all neurons
-i_nrn = 102
-i_trl = 0
-fig, ax = plt.subplots(2, 1)
-for i in range(len(dat_st[0]['ss'][:][i_trl])):
-    col = '.r' if i == i_nrn else '.k'
-    ax[0].plot(dat_st[0]['ss'][i][i_trl], np.ones(len(dat_st[0]['ss'][i][i_trl]))*i, col)
-ax[0].set_ylabel('neuron index')
-# test: plot spikes from first session, all trials for a specific neuron
-for i, trl in enumerate(dat_st[0]['ss'][i_nrn]):
-    col = '.r' if i == i_trl else '.k'
-    ax[1].plot(trl, np.ones(len(trl))*i, col)
-ax[1].set_title('neuron #' + str(i_nrn))
-ax[1].set_ylabel('trial index')
-[a.set_xlabel('time [s]') for a in ax]
+# spike raster plot for one trial (all neurons) and one neuron (all trials)
+i_trial = 0
+i_neuron = 102
+ax = plots.raster(spiketimes, i_trial=i_trial, i_neuron=i_neuron)
+
+plt.show()
